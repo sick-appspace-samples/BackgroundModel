3ee863134304bc92d40bf0cc38845856a61bc7e1
SICKAppSpace-dev <>
2023-12-20 23:52:46 +0000

    Update

--- BackgroundModel/scripts/BackgroundModel.lua
+++ BackgroundModel/scripts/BackgroundModel.lua
@@ -33,28 +33,33 @@ local backgroundModel = Image.BackgroundModel.createRunningGaussian(lineScanSens
 
 -- Create a viewer and a decorator that spans 0 to 100 degrees celsius
 local viewer = View.create()
-local imagedecoration = View.ImageDecoration.create()
-imagedecoration:setRange(0, 100)
+local imagedecoration = View.ImageDecoration.create():setRange(0, 100)
 
 --End of Global Scope-----------------------------------------------------------
 
 --Start of Function and Event Scope---------------------------------------------
 
--- Handle each captured image
+---Handle each captured image
+---@param image Image
 local function callback(image)
-  
+
   -- Update background model with this new observation
   backgroundModel:add(image)
-  
+
   -- Use model to get parts of the image that don't belong
   local fg = backgroundModel:compare(image, "BRIGHTER", meanThreshold, varianceThreshold)
-  
+
   -- Get the model content
   local modelImages = backgroundModel:getModelImages()
-  
-  -- Function used to build a visualization image
-  -- This will concatenate the input image with any provided model images
+
+  ---Function used to build a visualization image
+  ---This will concatenate the input image with any provided model images
+  ---@param input Image
+  ---@param modelImagesV Image[]
+  ---@return Image
   local function createDisplayImage(input, modelImagesV)
+    ---@param inputI Image
+    ---@return Image
     local function toFloat(inputI)
       local inputF = Image.toType(inputI, "FLOAT32")
       local _,_,pixelSizeZ = Image.getPixelSize(inputI)
@@ -64,10 +69,10 @@ local function callback(image)
       inputF:setOriginZ(0.0)
       return inputF
     end
-  
+
     -- Convert the source image to a float with unitary coordinates
     input = toFloat(input)
-    
+
     -- Concatenate all images but the last into one for easy display
     -- The first model image is the average value of each pixel in the model
     -- The second model image (if it exists) is the variance of each pixel in the model
@@ -77,48 +82,49 @@ local function callback(image)
     end
     return input
   end
-  
+
   local displayImage = createDisplayImage(image, modelImages)
-  
+
   -- Display a visualization of the model
   viewer:clear()
-  local imid = viewer:addImage(displayImage, imagedecoration)
-  viewer:addPixelRegion(fg, nil, nil, imid)
+  viewer:addImage(displayImage, imagedecoration)
+  viewer:addPixelRegion(fg)
   viewer:present()
 end
 
 local function main()
-  
+
   -- Use this simple function to keep the framerate
   -- we could also have used a Timer object.
   local tic = DateTime.getTimestamp()
+  ---@param hz int
   local function pace(hz)
     local toc = DateTime.getTimestamp()
     local sleeptime = 1000/hz - (toc-tic)
     Script.sleep(math.max(0, sleeptime))
     tic = toc
   end
-  
+
   -- This image set was collected at 6 Hz
   local hz = 6
   local images = Object.load('resources/area.json')
-  
+
   -- Loop a few iterations
   for k = 1, 10 do
-  
+
     -- Increment
     for imageIndex = 1, #images do
         callback(images[imageIndex])
         pace(hz)
     end
-    
+
     -- Decrement
     for imageIndex = #images-1, 2, -1 do
         callback(images[imageIndex])
         pace(hz)
     end
   end
-  
+
   print('App finished.')
 end
 --The following registration is part of the global scope which runs once after startup
